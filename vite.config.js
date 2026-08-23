46b04356dbeb52d89e949fcdbf6f8981a7472187
Tayyab Sabir <141709147+tayyab1251@users.noreply.github.com>
2026-08-23 13:02:22 +0500

    build: update vite configurations

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,32 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',    
+
+                // Theme files for including in head
+                'resources/assets/libs/bootstrap/css/bootstrap.min.css',
+                'resources/assets/libs/bootstrap-icons/bootstrap-icons.css',
+                'resources/assets/libs/apexcharts/apexcharts.css',
+                'resources/assets/libs/flatpickr/flatpickr.min.css',
+                'resources/assets/css/main.css',
+
+                // Theme Javascript files
+                'resources/assets/libs/bootstrap/js/bootstrap.bundle.min.js',
+                'resources/assets/libs/apexcharts/apexcharts.min.js',
+                'resources/assets/libs/flatpickr/flatpickr.min.js',
+                'resources/assets/js/dashboard.js',
+
+                // Javascript files
+                'resources/js/app.js',
+
+            ],
+            assets: ['resources/assets/images/**'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
                     weights: [400, 500, 600],
+                    optimizedFallbacks: false,
                 }),
             ],
         }),
